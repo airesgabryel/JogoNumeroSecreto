0dfed2dbe6e0339f12e67c23b5b9585564c839e4
AiresGabryel <56Gabryel@gmail.com>
2025-07-28 01:05:13 -0300

    wrong answer treatment 2

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ let numeroSecreto = 9;
 console.log(numeroSecreto)
 let chute = prompt('Escolha um número entre 1 e 10');
 
-// se chute for igual ao número secreto
+
 if (chute == numeroSecreto) {
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    if(numeroSecreto > chute ){
+    if(chute < numeroSecreto) {
         alert(`Você errou, o numero secreto é maior que ${chute}`)
     } else {
         alert(`Você errou, o número secreto é menor que ${chute}`)
