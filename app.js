7f5484c1b4e85010325fad89956a7f310d949ec5
AiresGabryel <56Gabryel@gmail.com>
2025-07-28 01:04:24 -0300

    wrong answer treatment

--- app.js
+++ app.js
@@ -7,5 +7,9 @@ let chute = prompt('Escolha um número entre 1 e 10');
 if (chute == numeroSecreto) {
     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
 } else {
-    alert('Você errou :(')
+    if(numeroSecreto > chute ){
+        alert(`Você errou, o numero secreto é maior que ${chute}`)
+    } else {
+        alert(`Você errou, o número secreto é menor que ${chute}`)
+    }
 }
