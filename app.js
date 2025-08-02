de23bfc70c62c7e8b7f1fe8f6270e2f2a557bdfa
AiresGabryel <56Gabryel@gmail.com>
2025-08-02 03:07:17 -0300

    ternary operator

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
 alert('Boas vindas ao jogo do número secreto');
-let numeroSecreto = parseInt(Math.random() * 10 + 1);
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
+//math random gera números aleatórios entre 0 e 1.
+// gera um número entre 0 a 9 ("+1" no cógigo gera entre 1 a 10)
+// * 10 gera número inteiro e quebrados, (*10) <- menor que isso gera número decimal
+//parseInt transforma o número em int, removendo casas decimais após a vírgula.
+
+// * 100 vai de 0 a 99, com +1 gera de 1 a 100
 console.log(numeroSecreto)
 let chute;
 let tentativas = 1;
 
+
+
+
+
+
+
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt('Escolha um número entre 1 e 100');
     // se chute for igual ao número secreto
     if (chute == numeroSecreto) {
        break;
@@ -17,9 +29,10 @@ while (chute != numeroSecreto) {
             alert(`O número secreto é maior que ${chute}`)
         }
         // tentativas = tentativas + 1
-        tentativas++
+        tentativas++;
     }
 }
 
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+// Usa um operador ternário para verificar: se o número de tentativas for maior que 1, a variável recebe 'tentativas' (plural); senão, recebe 'tentativa' (singular).
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
 alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
