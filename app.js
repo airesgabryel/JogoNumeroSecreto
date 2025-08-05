7f9904c7098c93ad0b2a33d9fd074bf83f5e1d3d
AiresGabryel <56Gabryel@gmail.com>
2025-08-04 23:21:27 -0300

    alterando saída de texto

--- app.js
+++ app.js
@@ -11,12 +11,6 @@ console.log(numeroSecreto)
 let chute;
 let tentativas = 1;
 
-
-
-
-
-
-
 // enquanto chute não for igual ao n.s.
 while (chute != numeroSecreto) {
     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo} `);
@@ -25,9 +19,9 @@ while (chute != numeroSecreto) {
        break;
     } else {
         if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que: ${chute}`)
+            alert(`O número secreto é menor que ${chute}`)
         } else {
-            alert(`O número secreto é maior que: ${chute}`)
+            alert(`O número secreto é maior que ${chute}`)
         }
         // tentativas = tentativas + 1
         tentativas++;
