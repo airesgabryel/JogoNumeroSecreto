ceb2a76980399095d6eba1d49f11851ced1884a4
Gabryel Aires <56Gabryel@gmail.com>
2025-10-02 23:48:45 -0300

    Update app.js
    alterando comentários

--- app.js
+++ app.js
@@ -11,10 +11,10 @@ console.log(numeroSecreto)
 let chute;
 let tentativas = 1;
 
-// enquanto chute não for igual ao n.s.
+// enquanto chute não for igual ao número secreto.
 while (chute != numeroSecreto) {
     chute = prompt(`Escolha um número entre 1 e ${numeroMaximo} `);
-    // se chute for igual ao número secreto
+    // if chute for igual ao número secreto
     if (chute == numeroSecreto) {
        break;
     } else {
